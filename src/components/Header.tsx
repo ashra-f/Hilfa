634fcedf5fa3b36ec076487e25901df53dd05d2e
“ashra-f” <ashrafhammoud2@gmail.com>
2024-01-11 22:15:50 -0500

    Add favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,24 @@
-import type { Component } from "solid-js";
-import logo from "../assets/logo.png";
-import instagramLogo from "../assets/instagram-logo.png"; 
+import type { Component } from "solid-js"
+import logo from "../assets/dark-mode-logo.png"
+import instagramLogo from "../assets/instagram-logo.png"
 
 const Header: Component = () => {
   return (
     <header class="flex items-center justify-between p-2 shadow-md">
       <a href="/">
-        <img src={logo.src} alt="Logo" class="h-16" /> 
+        <img src={logo.src} alt="Logo" class="h-16" />
       </a>
       <div class="flex items-center">
-        <a href="https://www.instagram.com/hilfastudios" target="_blank" rel="noopener noreferrer">
-          <img src={instagramLogo.src} alt="Instagram" class="h-5" /> 
+        <a
+          href="https://www.instagram.com/hilfastudios"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagramLogo.src} alt="Instagram" class="h-5" />
         </a>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
