149ae447f2c316901c9247e486da760555343bc0
“ashra-f” <ashrafhammoud2@gmail.com>
2024-01-12 01:35:57 -0500

    clickable product cards

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,22 +11,22 @@ interface Props {
 
 const ProductCard: Component<Props> = (props) => {
   return (
-    <li class="max-w-xs mx-auto bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl">
-      <a href={props.href} class="block relative">
-        <img
-          src={props.image}
-          alt={props.title}
-          class="object-cover object-top w-full h-64"
-        />
-      </a>
-      <div class="mt-4 p-4">
-        <a href={props.href} class="text-gray-900 font-semibold text-lg">
-          {props.title}
-        </a>
-        <p class="mt-2 text-gray-600 text-sm">{props.body}</p>
-        <p class="mt-2 text-gray-900 font-bold text-xl">${props.price}</p>
-      </div>
-    </li>
+    <a href={props.href} class="block">
+      <li class="max-w-xs mx-auto bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl">
+        <div class="block relative">
+          <img
+            src={props.image}
+            alt={props.title}
+            class="object-cover object-top w-full h-64"
+          />
+        </div>
+        <div class="mt-4 p-4">
+          <span class="text-gray-900 font-semibold text-lg">{props.title}</span>
+          <p class="mt-2 text-gray-600 text-sm">{props.body}</p>
+          <p class="mt-2 text-gray-900 font-bold text-xl">${props.price}</p>
+        </div>
+      </li>
+    </a>
   )
 }
 
