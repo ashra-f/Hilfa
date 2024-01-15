6793fc65f2d51a8e7f83bc89744b1f1867f0afdb
“ashra-f” <ashrafhammoud2@gmail.com>
2024-01-14 23:38:11 -0500

    compress files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config"
 import solidJs from "@astrojs/solid-js"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
+import compress from "astro-compress"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  integrations: [solidJs(), tailwind(), compress()],
   output: "server",
   adapter: vercel(),
 })
